8fd5fdbce9cc9a8652d44772ba712077a79757c5
jenkins <jenkins@rongcloud.net>
2025-06-05 09:59:24 +0800

    Release IM SDK Version 5.20.1

--- Package.swift
+++ Package.swift
@@ -21,48 +21,48 @@ let package = Package(
     targets: [
     	.binaryTarget(
             name: "RongIMLibCore",
-            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_IMLibCore_SDK_v5_20_0_Dev.zip",
-            checksum: "1a0367d3f47d9f507cdd7877fc7c73753a1c345568d581aca8cf40ee90d7235d"
+            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_IMLibCore_SDK_v5_20_1_Dev.zip",
+            checksum: "960c423dd6b0945db89264c8aafe9aad610247ee595fd6cdc29e743ab53ac840"
         ),
         .binaryTarget(
             name: "RongChatRoom",
-            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_ChatRoom_SDK_v5_20_0_Dev.zip",
-            checksum: "8df3e7c7e6acdb452429a2a07e41aeab5e439f1aa89869ddf2e2a47805271b33"
+            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_ChatRoom_SDK_v5_20_1_Dev.zip",
+            checksum: "c982a3200692394f02703cf07c2ee8c1016ca67e5f4d0a6c6e52639ab7fe56e9"
         ),
         .binaryTarget(
             name: "RongCustomerService",
-            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_CustomerService_SDK_v5_20_0_Dev.zip",
-            checksum: "f390518b5be600833e0b61ff19e962e55b2f3dded3f9995de89e1e66169cff2c"
+            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_CustomerService_SDK_v5_20_1_Dev.zip",
+            checksum: "1c5ae6b77ca442547448db6b0992474e8537623570182a0805c5f25de5d74084"
         ),
         .binaryTarget(
             name: "RongDiscussion",
-            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_Discussion_SDK_v5_20_0_Dev.zip",
-            checksum: "8b6bee5a10070022618191cc18c641b5c2a7e4317dbba3c5494eb40a33fca9f4"
+            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_Discussion_SDK_v5_20_1_Dev.zip",
+            checksum: "6037b80533360af355c123260bb561c81fe48b7c10bf3118cb14fd1ea54a6de8"
         ),
         .binaryTarget(
             name: "RongPublicService",
-            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_PublicService_SDK_v5_20_0_Dev.zip",
-            checksum: "c250485e2809f87334b20e9c9df57adc5a1b1cb419b5cca61410becb8e78d079"
+            url: "https://downloads.rongcloud.cn/Rong_Cloud_iOS_PublicService_SDK_v5_20_1_Dev.zip",
+            checksum: "7d556e8f68859f2ab2bda9bac7542c170443462f6438ababf84529bced603f25"
         ),
         .binaryTarget(
             name: "RongIMKit",
-            url: "https://downloads.rongcloud.cn/Rong_Cloud_SPM_iOS_IMKit_SDK_v5_20_0_Dev.zip",
-            checksum: "05eb9718ff63f1b538c114907a8a76749613b7400f62aea12feab52c9a95448f"
+            url: "https://downloads.rongcloud.cn/Rong_Cloud_SPM_iOS_IMKit_SDK_v5_20_1_Dev.zip",
+            checksum: "3078480fd61319091dece188393eb83b12f5dd5c1745f2c60d9c9b18ce540147"
         ),
         .binaryTarget(
             name: "RongSticker",
-            url: "https://downloads.rongcloud.cn/Rong_Cloud_SPM_iOS_Sticker_SDK_v5_20_0_Dev.zip",
-            checksum: "6d47ff9af72673831b5d5708b42aefe6bae1cd8d007ab00e1e4e80ff3ee589f4"
+            url: "https://downloads.rongcloud.cn/Rong_Cloud_SPM_iOS_Sticker_SDK_v5_20_1_Dev.zip",
+            checksum: "3eb702feba493cd2d12396ff75dacde81fe328dba1cf9a8d6d2ffe420aa3c03f"
         ),
         .binaryTarget(
             name: "RongSight",
-            url: "https://downloads.rongcloud.cn/Rong_Cloud_SPM_iOS_Sight_SDK_v5_20_0_Dev.zip",
-            checksum: "04d6617f966f75102ca31965e5706f361892d33b661579e97b63791fbde2ee22"
+            url: "https://downloads.rongcloud.cn/Rong_Cloud_SPM_iOS_Sight_SDK_v5_20_1_Dev.zip",
+            checksum: "fbd01ddb6572356dcce9f71190c60d194623e585136a92be71b562a365ef576b"
         ),
         .binaryTarget(
             name: "RongContactCard",
-            url: "https://downloads.rongcloud.cn/Rong_Cloud_SPM_iOS_ContactCard_SDK_v5_20_0_Dev.zip",
-            checksum: "46012dbf872a7a6faed3718929cb85723446201dab314ace9be4936b98723f58"
+            url: "https://downloads.rongcloud.cn/Rong_Cloud_SPM_iOS_ContactCard_SDK_v5_20_1_Dev.zip",
+            checksum: "f829a437e7c792610d969c6b60cf95201f52a21f85b776deab0603b3c8cc0c76"
         )
     ]
 )
